404cacb9271e54a11f88ce7255b5f5e604ab7137
jijiseong <jiseong5036@gmail.com>
2025-10-28 16:23:07 +0900

    feat: 녹화 고정 시간 time 보여주기

--- src/routes/(home)/-components/timer-section.tsx
+++ src/routes/(home)/-components/timer-section.tsx
@@ -2,14 +2,8 @@ import { Flex, Text } from '@radix-ui/themes';
 import { formatDate } from 'date-fns';
 
 import { useRecordingInfoQuery } from '@/routes/(home)/-hook/use-recording-info-query';
-import { useIntervalNow } from '@/hooks/use-now';
 
 export default function Timer() {
-  const { outOfOperatingTime, baseInfo, isOverHalf } = useRecordingInfoQuery();
-
-  if (!baseInfo?.isRecording && isOverHalf) return null;
-  if (outOfOperatingTime) return null;
-
   return (
     <Flex mx="auto" width="100%" justify="between" gap="1" align="center">
       <StartTimer />
@@ -20,26 +14,14 @@ export default function Timer() {
 }
 
 function StartTimer() {
-  const { baseInfo } = useRecordingInfoQuery();
-
-  if (!baseInfo?.isRecording) return <NowTimer />;
+  const { currentRecordingStartDate } = useRecordingInfoQuery();
 
   return (
     <Text size="6" weight="bold">
-      {formatDate(baseInfo?.recording.triggeredAt, 'HH시 mm분')}
+      {formatDate(currentRecordingStartDate, 'HH시 mm분')}
     </Text>
   );
 }
-
-function NowTimer() {
-  const { now } = useIntervalNow({ interval: 1000 });
-  return (
-    <Text size="6" weight="bold">
-      {formatDate(now, 'HH시 mm분')}
-    </Text>
-  );
-}
-
 function EndTimer() {
   const { currentRecordingEndDate } = useRecordingInfoQuery();
 
