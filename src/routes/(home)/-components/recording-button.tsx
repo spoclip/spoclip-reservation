526f6ad6e195b1315570349c8ea9d47a59fb8ce6
jijiseong <jiseong5036@gmail.com>
2025-10-28 16:16:52 +0900

    feat: 녹화하기 버튼 조건부 렌더링 삭제

--- src/routes/(home)/-components/recording-button.tsx
+++ src/routes/(home)/-components/recording-button.tsx
@@ -1,12 +1,6 @@
 import { Box, Button } from '@radix-ui/themes';
 
-import { useRecordingInfoQuery } from '@/routes/(home)/-hook/use-recording-info-query';
-
 function RecordingButton() {
-  const { baseInfo, isOverHalf, outOfOperatingTime } = useRecordingInfoQuery();
-
-  if (baseInfo?.isRecording || isOverHalf || outOfOperatingTime) return;
-
   return (
     <Box flexGrow="1" asChild>
       <Button type="submit" size="4">
